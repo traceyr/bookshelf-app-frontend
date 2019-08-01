d580ca03b2fc3a6bc9b88d565ef1366042004151
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-07-31 17:15:57 -0700

    got react to connect to express

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    getBasicBitchInfo: ''
+  }
+
+  componentDidMount() {
+    this.callBasicInfo()
+      .then(res => this.setState({ getBasicBitchInfo: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callBasicInfo = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if(response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
   render() {
-    return <div>I am ready to use the back end api!!!!</div>
+    return (
+      <div>
+        <div>I am ready to use the back end api!!!!</div>
+        <p>{ this.state.getBasicBitchInfo }</p>
+      </div>
+    );
   }
 }
 
