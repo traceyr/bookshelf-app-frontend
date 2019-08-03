f59103f42eed41508c9e7bfd56fc42b92cfc8af2
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-08-02 20:11:27 -0700

    created weback file

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import BookList from './BookList';
-import books from '../apis/books.js';
 import axios from 'axios';
 
 class App extends Component {
